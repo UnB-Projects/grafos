06d14c12282667a31e743600e694e47e43c91219
igorfg <igorfgds@gmail.com>
2017-09-03 10:21:39 -0300

    Comentarios inseridos no trabalho, pronto para entrega

--- Trabalho 1/grafo.cpp
+++ Trabalho 1/grafo.cpp
@@ -1,35 +1,43 @@
+//Trabalho 1 da Disciplina Teoria e Aplicação de Grafos - Turma A
+//Integrantes
+//Nome: Igor Figueira Pinheiro da Silva
+//Matricula: 15/0129921
+
+//Nome: Tiago Rodrigues da Cunha Cabral
+//Matricula: 15/0150296
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 #define MAX 49
 
-typedef struct Vertice {
-  string Nome;
-  int pk;
-  vector<int> amigos;
+typedef struct Vertice { //Definicao da estrutura de um vertice
+  string Nome;           //Nome dos alunos
+  int pk;                //Identificador unico de cada vertice
+  vector<int> amigos;    //Lista de conexoes (arestas)
 }t_Vertice;
 
-typedef vector<Vertice> Grafo;
+typedef vector<Vertice> Grafo; //Um grafo eh definido como um conjunto de vertices
 
-//prototipos
+//Prototipos de funcoes
 
-bool comparaAmigos(Vertice, Vertice);
-bool comparaPK(Vertice, Vertice);
-void mostra_grafo(Grafo);
 vector<string> leitura_arquivo();
 void adicionaVertice(Grafo&, string, int);
 void adicionaAresta(Grafo&, string, int);
 void adicionaNome(Grafo&, string, int);
 void montaGrafo(Grafo&);
+void mostra_grafo(Grafo);
 void mostraGrafoDecrescente(Grafo);
+void mostraCliques();
+void bronKerbosch (Grafo, Grafo, Grafo, Grafo);
 Grafo uniaoVertices(Grafo, Grafo);
 Grafo intersecaoVertices(Grafo, Grafo);
 Grafo complementarVertices(Grafo, Grafo);
 Grafo vizinhosVertice(Grafo, Vertice);
-void bronKerbosch (Grafo, Grafo, Grafo, Grafo);
+bool comparaAmigos(Vertice, Vertice);
+bool comparaPK(Vertice, Vertice);
 bool comparaCliques(Grafo, Grafo);
-void mostraCliques();
 
 //Grafo de cliques maximos
 
@@ -39,28 +47,20 @@ int main () {
   Grafo g(MAX);
   Grafo R;
   Grafo X;
-  Grafo teste;
-  Grafo teste2;
-  Grafo teste3;
-  Vertice testeVertice;
 
   montaGrafo(g);
 
-  cout << "Inicio mostra_grafo" << endl;
+  cout << "////// Listas de Adjacencias //////" << endl << endl;
   mostra_grafo(g);
-  cout << "Fim mostra_grafo" << endl << endl;
 
-  cout << "Inicio mostra_grafo decrescente" << endl;
+  cout << endl << "////// Grafo ordenado por numero de amigos //////" << endl << endl;
   mostraGrafoDecrescente(g);
-  cout << "Fim mostra_grafo decrescente" << endl << endl;
 
   bronKerbosch(g, R, g, X);
-
-  sort(maximalCliques.begin(), maximalCliques.end(), comparaCliques);
-
   mostraCliques();
 }
 
+//Funcao de leitura do arquivo
 vector<string> leitura_arquivo() {
   vector<string> linhas(50);
   string linha;
@@ -107,7 +107,7 @@ void montaGrafo(Grafo &g) {
         ch.clear();
       }
       else if (linhas[i][j] >= 'A' && linhas[i][j] <= 'Z') { // Se for caractere
-        // Loop que coloca em uma string auxiliar ch todos os caracteres, até o final do arquivo
+        // Loop que coloca em uma string auxiliar ch, todos os caracteres até o final do arquivo
         for (k = j; k <(int)linhas[i].size(); k++) {
           ch.push_back(linhas[i][k]);
         }
@@ -119,19 +119,19 @@ void montaGrafo(Grafo &g) {
   }
 }
 
-void adicionaVertice(Grafo &g, string ch, int i) {
+void adicionaVertice(Grafo &g, string ch, int i) { //Funcao que adiciona um vertice a lista de vertices
   g[i].pk = atoi(ch.c_str());
 }
 
-void adicionaAresta(Grafo &g, string ch, int i) {
+void adicionaAresta(Grafo &g, string ch, int i) { //Funcao que adiciona um vertice a uma lista de adjacencias de outros vertice, formando uma aresta
   g[i].amigos.push_back(atoi(ch.c_str()));
 }
 
-void adicionaNome(Grafo &g, string ch, int i) {
+void adicionaNome(Grafo &g, string ch, int i) { //Funcao que adiciona o atributo nome de determinado vertice
   g[i].Nome = ch;
 }
 
-void mostra_grafo(Grafo g) {
+void mostra_grafo(Grafo g) { //Funcao que recebe um grafo e o imprime na tela
   int size = g.size();
   int size2;
   int i, j;
@@ -156,18 +156,20 @@ void mostra_grafo(Grafo g) {
 void mostraGrafoDecrescente(Grafo g) {
   int i;
 
-  sort(g.begin(), g.end(), comparaAmigos);
+  sort(g.begin(), g.end(), comparaAmigos); //Ordenacao de grafo por grau dos vertices
   for (i = 0; i < (int)g.size(); i++) {
     cout << "Vertice: " << setw(2) << g[i].pk << " " 
          << "Grau: "    << setw(2) << g[i].amigos.size() << endl;
   }
 }
 
-void mostraCliques() {
+void mostraCliques() { //Funcao que mostra dois dos cliques guardados na variavel maximalCliques
   int i;
 
-  cout << "Clique Maximal do grafo por nomes:" << endl << endl;
-  for (i = 0; i < (int)maximalCliques[8].size(); i++) {
+  sort(maximalCliques.begin(), maximalCliques.end(), comparaCliques); //Ordenacao da lista de cliques por numero de ligacoes simultaneas em cada clique
+
+  cout << endl << "////// Clique maximal do grafo por nomes //////" << endl << endl;
+  for (i = 0; i < (int)maximalCliques[8].size(); i++) { //O clique 8 foi escolhido como representante de um clique maximal
     if (i > 0) {
       cout << " - ";
     }
@@ -175,8 +177,8 @@ void mostraCliques() {
   }
   cout << endl << endl;
 
-  cout << "Clique Maximo do grafo por nomes:" << endl << endl;
-  for (i = 0; i < (int)maximalCliques[0].size(); i++) {
+  cout << "////// Clique Maximo do grafo por nomes //////" << endl << endl;
+  for (i = 0; i < (int)maximalCliques[0].size(); i++) { //O clique 0 foi escolhido como exemplo de um clique maximo
     if (i > 0) {
       cout << " - ";
     }
@@ -185,7 +187,7 @@ void mostraCliques() {
   cout << endl;
 }
 
-void bronKerbosch (Grafo g, Grafo R, Grafo P, Grafo X) {
+void bronKerbosch (Grafo g, Grafo R, Grafo P, Grafo X) { //Implementacao do algoritmo de BronKerbosch com pivotamento
   Vertice pivo;
   Grafo uniao;
   Grafo vizinhosPivo;
@@ -193,36 +195,36 @@ void bronKerbosch (Grafo g, Grafo R, Grafo P, Grafo X) {
   Grafo conjuntoV;
   int i = 0;
 
-  if (P.empty() && X.empty()) {
+  if (P.empty() && X.empty()) { //Caso o conjunto de vertices P e X estejam vazios, o conjunto de vertices R eh um clique
     maximalCliques.push_back(R);
     return;
   }
 
-  uniao = uniaoVertices(P, X);
-  sort(uniao.begin(), uniao.end(), comparaAmigos);
-  pivo = uniao[0];
+  uniao = uniaoVertices(P, X); //A variavel uniao recebe a uniao do conjunto de vertices P e X
+  sort(uniao.begin(), uniao.end(), comparaAmigos); //Lista de uniao eh ordenada de acordo com o grau de cada vertice
+  pivo = uniao[0]; //O pivo selecionado eh o vertice de maior grau
 
-  vizinhosPivo = vizinhosVertice(g, pivo);
+  vizinhosPivo = vizinhosVertice(g, pivo); //vizinhosPivo recebe todos os vertices que estao na lista de adjacencia do pivo
 
 
-  verticesComplementares = complementarVertices(P, vizinhosPivo);
+  verticesComplementares = complementarVertices(P, vizinhosPivo); //verticesComplementares recebe o conjunto de vertices complementares P (complementar) Vizinhos(pivo)
 
-  for (i = 0; i < (int)verticesComplementares.size(); i++) {
-    conjuntoV.push_back(verticesComplementares[i]);
+  for (i = 0; i < (int)verticesComplementares.size(); i++) { //Havera uma chamada recursiva de bronKerbosch para cada vertice no conjunto complementar P \ Vizinhos(pivo)
+    conjuntoV.push_back(verticesComplementares[i]); //O vertice da atual iteracao eh encapsulado em um conjunto de vertices para ser usado nas funcoes de conjunto
 
-    bronKerbosch(g, uniaoVertices(R, conjuntoV),
-                 intersecaoVertices(P, vizinhosVertice(g, verticesComplementares[i])),
-                 intersecaoVertices(X, vizinhosVertice(g, verticesComplementares[i])));
+    bronKerbosch(g, uniaoVertices(R, conjuntoV), //Cada chamada recursiva de bronKerbosch recebe o grafo principal para consultas, (R (uniao) vertice atual do loop)
+                 intersecaoVertices(P, vizinhosVertice(g, verticesComplementares[i])), // P (intersecao) vizinhos do vertice atual do loop
+                 intersecaoVertices(X, vizinhosVertice(g, verticesComplementares[i])));// X (intersecao) vizinhos do vertice atual do loop
 
-    P = complementarVertices(P, conjuntoV);
-    X = uniaoVertices(X, conjuntoV);
+    P = complementarVertices(P, conjuntoV); //No final das chamadas recursivas do loop, P recebe P (complementar) vertice atual do loop
+    X = uniaoVertices(X, conjuntoV); // X recebe X (uniao) vertice atual do loop
 
-    conjuntoV.clear();
+    conjuntoV.clear(); //conjuntoV eh esvaziado para ser conter apenas um vertice no loop seguinte
   }
 }
 
 //Funcoes auxiliares de Bron Kerbosch
-Grafo uniaoVertices(Grafo x, Grafo y) {
+Grafo uniaoVertices(Grafo x, Grafo y) { //Funcao que recebe dois conjuntos de vertices e retorna a uniao deles
   Grafo uniao;
   int i, j;
 
@@ -232,7 +234,7 @@ Grafo uniaoVertices(Grafo x, Grafo y) {
   j = 0;
 
   while (i < (int)x.size() && j < (int)y.size()) {
-    if (y[j].pk < uniao[i].pk) {
+    if (y[j].pk < uniao[i].pk) { //O numero identificar de cada vertice eh utilizado na escolha dos vertices da uniao
       uniao.push_back(y[j]);
       j++;
     }
@@ -255,14 +257,14 @@ Grafo uniaoVertices(Grafo x, Grafo y) {
   return uniao;
 }
 
-Grafo intersecaoVertices(Grafo x, Grafo y) {
+Grafo intersecaoVertices(Grafo x, Grafo y) { //Funcao que recebe dois conjuntos de vertices e retorna a intersecao deles
   Grafo intersecao;
   int i, j;
 
   i = 0;
   j = 0;
   while (i < (int)x.size() && j < (int)y.size()) {
-    if (x[i].pk < y[j].pk) {
+    if (x[i].pk < y[j].pk) { //O numero identificar de cada vertice eh utilizado na escolha dos vertices da intersecao
       i++;
     }
     else if (x[i].pk > y[j].pk) {
@@ -278,7 +280,7 @@ Grafo intersecaoVertices(Grafo x, Grafo y) {
   return intersecao;
 }
 
-Grafo complementarVertices(Grafo x, Grafo y) {
+Grafo complementarVertices(Grafo x, Grafo y) { //Funcao que recebe dois conjuntos de vertices e retorna primeiro (complementar) segundo
   Grafo intersecao;
   Grafo complementar;
   int i, j;
@@ -288,7 +290,7 @@ Grafo complementarVertices(Grafo x, Grafo y) {
   i = 0;
   j = 0;
   while (i < (int)x.size() && j < (int)intersecao.size()) {
-    while (i < (int)x.size() && x[i].pk < intersecao[j].pk) {
+    while (i < (int)x.size() && x[i].pk < intersecao[j].pk) { //O numero identificar de cada vertice eh utilizado na escolha dos vertices complementares
       complementar.push_back(x[i]);
       i++;
     }
@@ -309,7 +311,7 @@ Grafo complementarVertices(Grafo x, Grafo y) {
   return complementar;
 }
 
-Grafo vizinhosVertice(Grafo g, Vertice v) {
+Grafo vizinhosVertice(Grafo g, Vertice v) { //Funcao que recebe o grafo princial para consulta e um vertice V, e retorna um conjunto de vertices com os vizinhos de v
   int i;
   Grafo vizinhos;
 
@@ -321,14 +323,14 @@ Grafo vizinhosVertice(Grafo g, Vertice v) {
 }
 
 //Funcoes de comparacao
-bool comparaAmigos(Vertice a, Vertice b) {
+bool comparaAmigos(Vertice a, Vertice b) { //Compara por grau (decrescente)
   return a.amigos.size() > b.amigos.size();
 }
 
-bool comparaPK(Vertice a, Vertice b) {
+bool comparaPK(Vertice a, Vertice b) {    //Compara por identificador (crescente)
   return a.pk < b.pk;
 }
 
-bool comparaCliques(Grafo a, Grafo b) {
+bool comparaCliques(Grafo a, Grafo b) {   //Compara por numero de vertices (decrescente)
   return a.size() > b.size();
 }
